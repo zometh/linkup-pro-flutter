7267652ec18c2091c5b4a1bcde5a24c78a364857
zometh <metzodiop45@gmail.com>
2025-10-21 00:00:05 +0000

    feat: Update backend URL, enhance Gradle configuration, and add Firebase support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,11 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.linkuppro"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,10 +26,10 @@ android {
         applicationId = "com.linkuppro"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
         multiDexEnabled = true
     }
     dependencies {
@@ -42,7 +43,9 @@ android {
         }
     }
 }
-
+dependencies(
+    implementation("com.google.firebase:firebase-messaging:23.3.0")
+)
 flutter {
     source = "../.."
 }
